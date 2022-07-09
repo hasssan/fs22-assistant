d55b706f4808ab1f31cd1bdc86c6fef9a79815ba
A. Hassan <git@hassan.web.id>
2022-07-09 16:45:25 +0700

    feat: show crops per month

---
+++ web/src/components/crop.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let name: String;
+</script>
+
+<div class="flex flex-col px-1 py-1 rounded-md">
+	<div class="text-center font-bold text-sm">
+		<span class="text-slate-800">
+			{name}
+		</span>
+	</div>
+</div>
--- web/src/components/month.svelte
+++ web/src/components/month.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Months } from '../types';
 	import ProductComponent from '../components/product.svelte';
+	import CropComponent from '../components/crop.svelte';
 
 	type SelectedMenu = 'product' | 'crop' | '';
 
@@ -36,16 +37,37 @@
 			</div>
 		{/if}
 	</div>
-	<div class="mb-3 mx-3 py-1 rounded-md bg-darkSeaGreen">
-		<div class="text-center cursor-pointer" on:click={() => selectMenu('crop')}>Crops</div>
-		{#if selectedMenu == 'crop'}
-			<div class="flex flex-wrap mt-3">
-				{#each month.products as product}
-					<div class="w-1/2">
-						<ProductComponent {product} monthName={month.name} />
+	{#if month.name != '-'}
+		<div class="mb-3 mx-3 rounded-md bg-darkSeaGreen ">
+			<div class="text-center py-1 cursor-pointer" on:click={() => selectMenu('crop')}>Crops</div>
+			{#if selectedMenu == 'crop'}
+				<div class="pb-1">
+					<div class="mt-3 bg-darkSeaGreen">
+						<div class="text-center font-bold">Plant</div>
+						<div class="flex flex-wrap">
+							{#if month?.plants?.length}
+								{#each month?.plants as item}
+									<div class="w-1/2">
+										<CropComponent name={item.name} />
+									</div>
+								{/each}
+							{/if}
+						</div>
 					</div>
-				{/each}
-			</div>
-		{/if}
-	</div>
+					<div class="my-3 bg-goldCrayola">
+						<div class="text-center font-bold">Harvest</div>
+						<div class="flex flex-wrap">
+							{#if month?.harvests?.length}
+								{#each month?.harvests as item}
+									<div class="w-1/2">
+										<CropComponent name={item.name} />
+									</div>
+								{/each}
+							{/if}
+						</div>
+					</div>
+				</div>
+			{/if}
+		</div>
+	{/if}
 </div>
