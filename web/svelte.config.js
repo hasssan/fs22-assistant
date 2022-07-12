ffde3de8a40f5a2122d8d7d5b75378ea96c79a84
A. Hassan <git@hassan.web.id>
2022-07-12 23:13:18 +0700

    build(web): use adapter-auto

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,18 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-		}),
-
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true,
-		},
+		adapter: adapter(),
 	},
 };
 
