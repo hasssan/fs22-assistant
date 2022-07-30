c31a6f514c21253003386eb4b33e9558cbf0db1d
A. Hassan <git@hassan.web.id>
2022-07-30 23:03:54 +0700

    feat(web): show Product Price as table

--- web/src/routes/products-price.svelte
+++ web/src/routes/products-price.svelte
@@ -1,46 +1,38 @@
 <script lang="ts">
 	import { products } from '../data';
 
-	import Product from '../components/product.svelte';
-
-	const monthData: { [key: string]: { [key: string]: any } } = {
-		jan: { name: 'Jan', products: [] },
-		feb: { name: 'Feb', products: [] },
-		mar: { name: 'Mar', products: [] },
-		apr: { name: 'Apr', products: [] },
-		may: { name: 'May', products: [] },
-		jun: { name: 'Jun', products: [] },
-		jul: { name: 'Jul', products: [] },
-		aug: { name: 'Aug', products: [] },
-		sep: { name: 'Sep', products: [] },
-		oct: { name: 'Oct', products: [] },
-		nov: { name: 'Nov', products: [] },
-		dec: { name: 'Dec', products: [] },
-		'-': { name: '-', products: [] },
-	};
-
-	const months: any[] = Object.values(monthData);
-
-	for (const key in monthData) {
-		const productOfMonth = products.filter((p) => p.months?.includes(key));
-		monthData[key].products = productOfMonth;
-		monthData[key].name_lower = monthData[key].name.toLowerCase();
+	function capitalizeMonths(months: string[]) {
+		return months.map((month) => {
+			return month[0].toLocaleUpperCase() + month.substring(1);
+		});
 	}
 </script>
 
-<h2 class="text-xl text-darkBrown font-bold">Sales Price by Month (Easy)</h2>
+<h2 class="text-xl text-darkBrown font-bold mb-8">Sales Price by Month (Easy)</h2>
 
-<div class="flex flex-wrap justify-between">
-	{#each months as month}
-		<div class="w-28">
-			<div class="text-center text-darkBrown font-bold">
-				{month.name}
-			</div>
-			<div class="text-center">
-				{#each monthData[month.name_lower].products as product}
-					<Product {product} monthName={month.name_lower} />
-				{/each}
-			</div>
-		</div>
-	{/each}
+<div class="w-2/3 overflow-auto">
+	<table class="table-auto w-full text-left whitespace-no-wrap">
+		<thead>
+			<tr class="text-green-800 bg-darkSeaGreen">
+				<th class="py-2 px-4">Product</th>
+				<th class="py-2 px-4">Best</th>
+				<th class="py-2 px-4">Good</th>
+				<th class="py-2 px-4">Average</th>
+				<th class="py-2 px-4">Month 1st</th>
+				<th class="py-2 px-4">Month 2nd</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each products as product}
+				<tr class="border-b border-darkSeaGreen">
+					<td class="px-6 py-1">{product.name}</td>
+					<td class="px-6 py-1">{product.best}</td>
+					<td class="px-6 py-1">{product.good}</td>
+					<td class="px-6 py-1">{product.average}</td>
+					<td class="px-6 py-1">{capitalizeMonths(product.first).join(', ')}</td>
+					<td class="px-6 py-1">{capitalizeMonths(product.second).join(', ')}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
